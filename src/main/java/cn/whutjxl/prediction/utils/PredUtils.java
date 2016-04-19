fde4be841be7cdfccdffa94fa05125d8cc95db61
whutjxl <whutjxl@WHUT-JXL>
2016-04-19 22:55:22 +0800

    dev_1构建框架

---
+++ src/main/java/cn/whutjxl/prediction/utils/PredUtils.java
@@ -0,0 +1,187 @@
+package cn.whutjxl.prediction.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class PredUtils {
+
+	public static List<Character> getFeaturesByExcel(File sourceFile) {
+		List<Character> features = new ArrayList<Character>();
+		Workbook workBook = null;
+		double previous = 0.0;
+		double current = 0.0;
+		double sub = 0.0;
+		try {
+			String fileName = sourceFile.getName();
+			if (fileName.endsWith(".xls")) {
+				workBook = new HSSFWorkbook(new FileInputStream(sourceFile));
+			} else if (fileName.endsWith(".xlsx")) {
+				workBook = new XSSFWorkbook(new FileInputStream(sourceFile));
+			}
+			Sheet sheet = workBook.getSheetAt(0);
+			Iterator<Row> it = sheet.rowIterator();
+			if (it.hasNext())
+				it.next();
+			if (it.hasNext())
+				previous = it.next().getCell(1).getNumericCellValue();
+			while (it.hasNext()) {
+				current = it.next().getCell(1).getNumericCellValue();
+				sub = current - previous;
+				previous = current;
+				if (sub > 0) {
+					features.add('U');
+				} else if (sub < 0) {
+					features.add('F');
+				} else {
+					features.add('-');
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (workBook != null) {
+				try {
+					workBook.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return features;
+	}
+
+	public static void createArff(List<Character> features, File destinationFile, int step, List<Integer> indexs) {
+		FileWriter writer = null;
+		StringBuffer sb = null;
+		try {
+			writer = new FileWriter(destinationFile);
+			String fileName = destinationFile.getName().split("\\.")[0];
+			writer.write("@relation " + fileName + "\n\n");
+			for (int i = step - 1; i >= 1; i--) {
+				if (indexs.contains(i)) {
+					writer.write("@attribute day" + i + " {U, -, F}\n");
+				}
+			}
+			writer.write("@attribute day0 {U, -, F}\n\n");
+			writer.write("@data\n");
+			int num = features.size();
+			if (num >= step) {
+				for (int j = step - 1; j < num; j++) {
+					sb = new StringBuffer();
+					for (int k = step - 1; k >= 1; k--) {
+						if (indexs.contains(k)) {
+							sb.append(features.get(j - k) + ",");
+						}
+					}
+					sb.append(features.get(j) + "\n");
+					writer.write(sb.toString());
+					writer.flush();
+				}
+			} else {
+				sb = new StringBuffer();
+				for (int j = step - 1; j >= num; j--) {
+					if (indexs.contains(j)) {
+						sb.append("?,");
+					}
+				}
+				for (int k = num - 1; k >= 1; k--) {
+					if (indexs.contains(k)) {
+						sb.append(features.get(num - 1 - k) + ",");
+					}
+				}
+				sb.append(features.get(num - 1) + "\n");
+				writer.write(sb.toString());
+				writer.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static Object trainModel(File sourceFile, String classifierName, String[][] options, boolean saveModel,
+			String originalName) {
+		String result = "";
+		Double pctCorrect = 0.0;
+		try {
+			DataSource source = new DataSource(new FileInputStream(sourceFile));
+			Instances train = source.getDataSet();
+			train.setClassIndex(train.numAttributes() - 1);
+			Classifier cls = (Classifier) Class.forName(classifierName).newInstance();
+			if (options != null) {
+				for (String[] strings : options) {
+					cls.setOptions(strings);
+				}
+			}
+			cls.buildClassifier(train);
+			Evaluation eval = new Evaluation(train);
+			eval.crossValidateModel(cls, train, 10, new Random(1));
+			String name = cls.getClass().getSimpleName();
+			if ("J48".equals(name) || "LADTree".equals(name) || "NBTree".equals(name)) {
+				result += cls.toString();
+			}
+			result += eval.toSummaryString();
+			pctCorrect = Double.parseDouble(String.format("%.2f", eval.pctCorrect()));
+			if (saveModel) {
+				String modelName = originalName + "_" + name + ".model";
+				String modelPath = "src/main/resources/models/";
+				SerializationHelper.write(modelPath + modelName, cls);
+				return result;
+			} else {
+				return pctCorrect;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String[] getPredictionResult(File sourceFile, String modelName) {
+		try {
+			DataSource dataSource = new DataSource(new FileInputStream(sourceFile));
+			Instances source = dataSource.getDataSet();
+			source.setClassIndex(source.numAttributes() - 1);
+			Instances prediction = new Instances(source);
+			String modelPath = "src/main/resources/models/" + modelName + ".model";
+			Classifier cls = (Classifier) SerializationHelper.read(modelPath);
+			int numInst = source.numInstances();
+			String[] results = new String[2];
+			double result = cls.classifyInstance(source.instance(numInst - 1));
+			prediction.instance(numInst - 1).setClassValue(result);
+			double pctCorrect = cls.distributionForInstance(source.instance(numInst - 1))[(int) result];
+			results[0] = prediction.instance(numInst - 1).stringValue(source.numAttributes() - 1);
+			results[1] = String.format("%.4f", pctCorrect);
+			return results;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
--- src/test/java/cn/whutjxl/prediction/PredictionUtilsTest.java
+++ src/test/java/cn/whutjxl/prediction/PredictionUtilsTest.java
@@ -1,16 +1,160 @@
 package cn.whutjxl.prediction;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.junit.Test;
+
+import cn.whutjxl.prediction.utils.PredUtils;
 import cn.whutjxl.prediction.utils.PredictionUtils;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class PredictionUtilsTest {
 
 	public static void main(String[] args) {
-		File sourceFile=new File("C:/Users/whutjxl/Desktop/stock.xlsx");
-		File destinationFile=new File("src/main/resources/arffs/stock.arff");
-//		PredictionUtils.getArffByExcel(sourceFile, destinationFile, 7, 0);
-		String s=(String) PredictionUtils.createModel(destinationFile, "weka.classifiers.trees.J48", null, true,"广电电气");
-		System.out.println(s);
+		File sourceFile = new File("C:/Users/whutjxl/Desktop/stock.xlsx");
+		File destinationFile = new File("src/main/resources/arffs/stock.arff");
+
+		for (int step = 4; step < 40; step++) {
+			PredictionUtils.getArffByExcel(sourceFile, destinationFile, step, 0, false);
+			Double s = (Double) PredictionUtils.createModel(destinationFile, "weka.classifiers.trees.J48", null, false,
+					"stock");
+			System.out.println(s);
+		}
 	}
+
+	@Test
+	public void fun1() {
+		/**
+		 * C:/Users/whutjxl/Desktop/stock.xlsx
+		 */
+		String classifierName = "weka.classifiers.trees.J48";
+		List<Character> features = PredUtils.getFeaturesByExcel(new File("C:/Users/whutjxl/Desktop/stock.xlsx"));
+		System.out.println(features);
+		List<Integer> indexs = new ArrayList<Integer>();
+
+		for (int step = 4; step < 14; step++) {
+
+			Map<Integer, Double> map = new HashMap<Integer, Double>();
+			for (int i = 1; i < step; i++) {
+				indexs.add(i);
+				PredUtils.createArff(features, new File("src/main/resources/stock.arff"), step, indexs);
+				Double result = (Double) PredUtils.trainModel(new File("src/main/resources/stock.arff"), classifierName,
+						null, false, "stock");
+				map.put(i, result);
+				indexs.clear();
+			}
+			for (Integer key : map.keySet()) {
+				if (map.get(key) > 50)
+					indexs.add(key);
+			}
+			System.out.println(map);
+			PredUtils.createArff(features, new File("src/main/resources/stock.arff"), step, indexs);
+			Double s = (Double) PredUtils.trainModel(new File("src/main/resources/stock.arff"), classifierName, null,
+					false, "stock");
+			System.out.println(s);
+			System.out.println(indexs);
+		}
+	}
+
+	@Test
+	public void fun2() {
+		String classifierName = "weka.classifiers.trees.J48";
+		for (int step = 8; step < 9; step++) {
+			PredictionUtils.getArffByExcel(new File("C:/Users/whutjxl/Desktop/stock.xlsx"),
+					new File("src/main/resources/stock.arff"), step, 0, false);
+			PredictionUtils.getArffByExcel(new File("C:/Users/whutjxl/Desktop/test.xlsx"),
+					new File("src/main/resources/test.arff"), step, 0, false);
+			try {
+				DataSource trainSource = new DataSource("src/main/resources/stock.arff");
+				DataSource testSource = new DataSource("src/main/resources/test.arff");
+				Instances train = trainSource.getDataSet();
+				Instances test = testSource.getDataSet();
+				train.setClassIndex(train.numAttributes() - 1);
+				test.setClassIndex(test.numAttributes() - 1);
+				Classifier cls = (Classifier) Class.forName(classifierName).newInstance();
+				cls.buildClassifier(train);
+				Evaluation eval = new Evaluation(train);
+				eval.evaluateModel(cls, test);
+				// System.out.println(cls.toString());
+				// System.out.println("===============");
+				// System.out.println(eval.toSummaryString());
+				// System.out.println(eval.toClassDetailsString());
+				// System.out.println(eval.toMatrixString());
+				System.out.println(eval.pctCorrect());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	@Test
+	public void fun3() {
+		String classifierName = "weka.classifiers.trees.J48";
+		List<Character> features = PredUtils.getFeaturesByExcel(new File("C:/Users/whutjxl/Desktop/stock.xlsx"));
+		List<Character> testList = new ArrayList<Character>();
+		for (int step = 4; step < 14; step++) {
+			int num = features.size();
+			for (int i = num * 9 / 10; i < num; i++) {
+				testList.add(features.get(i));
+			}
+			List<Integer> indexs = new ArrayList<Integer>();
+			Map<Integer, Double> map = new HashMap<Integer, Double>();
+			indexs.clear();
+			for (int i = 1; i < step; i++) {
+				indexs.add(i);
+				PredUtils.createArff(features, new File("src/main/resources/stock.arff"), step, indexs);
+				PredUtils.createArff(testList, new File("src/main/resources/test.arff"), step, indexs);
+				try {
+					DataSource trainSource = new DataSource("src/main/resources/stock.arff");
+					DataSource testSource = new DataSource("src/main/resources/test.arff");
+					Instances train = trainSource.getDataSet();
+					Instances test = testSource.getDataSet();
+					train.setClassIndex(train.numAttributes() - 1);
+					test.setClassIndex(test.numAttributes() - 1);
+					Classifier cls = (Classifier) Class.forName(classifierName).newInstance();
+					cls.buildClassifier(train);
+					Evaluation eval = new Evaluation(train);
+					eval.evaluateModel(cls, test);
+					map.put(i, eval.pctCorrect());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				indexs.clear();
+			}
+			System.out.println(map);
+			for (Integer key : map.keySet()) {
+				if (map.get(key) > 55)
+					indexs.add(key);
+			}
+			System.out.println(indexs);
+			PredUtils.createArff(features, new File("src/main/resources/stock.arff"), step, indexs);
+			PredUtils.createArff(testList, new File("src/main/resources/test.arff"), step, indexs);
+			try {
+				DataSource trainSource = new DataSource("src/main/resources/stock.arff");
+				DataSource testSource = new DataSource("src/main/resources/test.arff");
+				Instances train = trainSource.getDataSet();
+				Instances test = testSource.getDataSet();
+				train.setClassIndex(train.numAttributes() - 1);
+				test.setClassIndex(test.numAttributes() - 1);
+				Classifier cls = (Classifier) Class.forName(classifierName).newInstance();
+				cls.buildClassifier(train);
+				Evaluation eval = new Evaluation(train);
+				eval.evaluateModel(cls, test);
+				System.out.println(eval.pctCorrect());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
 }
