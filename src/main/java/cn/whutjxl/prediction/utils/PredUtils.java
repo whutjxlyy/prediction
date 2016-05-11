dfd10c19e271cdbb22ea108a0ce7d92d00681f53
whutjxl <whutjxl@WHUT-JXL>
2016-05-11 11:43:51 +0800

    准备定版本

--- src/main/java/cn/whutjxl/prediction/utils/PredUtils.java
+++ src/main/java/cn/whutjxl/prediction/utils/PredUtils.java
@@ -17,7 +17,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 import weka.core.converters.ConverterUtils.DataSource;
